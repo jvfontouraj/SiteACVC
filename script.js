8e8da08ea74286687db95efa1d6b5f89337f22f2
João Vitor Fontoura <jvfontouraj@gmail.com>
2023-02-11 12:30:14 -0300

    update

--- script.js
+++ script.js
@@ -4,12 +4,46 @@ const contador = document.querySelector('.contador')
 
 let i = 0
 
+let pastImg 
+let nextImg
+
 leftArrow.addEventListener("click", () => {
   i--
-  contador.textContent = i
+
+  if(i>=0 && i<5){
+    pastImg = document.querySelector(`.img-${i+1}`)
+    nextImg = document.querySelector(`.img-${i}`)
+
+    pastImg.classList.add("hide")
+    nextImg.classList.remove("hide")
+
+  } else if(i<0){
+    pastImg = document.querySelector('.img-0')
+    nextImg = document.querySelector('.img-4')
+
+    pastImg.classList.add("hide")
+    nextImg.classList.remove("hide")
+
+    i=4
+  }
 })
 
 rightArrow.addEventListener("click", () => {
   i++
-  contador.textContent = i
+
+  if(i>0 && i<5){
+    pastImg = document.querySelector(`.img-${i-1}`)
+    nextImg = document.querySelector(`.img-${i}`)
+
+    pastImg.classList.add("hide")
+    nextImg.classList.remove("hide")
+
+  } else{
+    i = 0
+    pastImg = document.querySelector('.img-4')
+    nextImg = document.querySelector('.img-0')
+
+    pastImg.classList.add("hide")
+    nextImg.classList.remove("hide")
+  }
 })
